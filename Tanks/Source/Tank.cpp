f3dbe6c96a3a8702f50277cab231f3ef412f15c0
aaronistheman2 <aarons.7007@gmail.com>
2014-07-24 10:19:35 -0700

    Lingering change

--- Tanks/Source/Tank.cpp
+++ Tanks/Source/Tank.cpp
@@ -29,14 +29,6 @@ Tank::Tank(Type type, const TextureHolder& textures)
 , mSprite(textures.get(toTextureID(type)))
 {
 	centerOrigin(mSprite);
-  /*
-  // Adjust the size of the sprite depending on type
-  switch (type)
-  {
-    case Tank::DefaultTank:
-      setSize(mSprite, sf::Vector2f(80.0f, 37.0f));
-      break;
-  }*/
 }
 
 void Tank::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const
